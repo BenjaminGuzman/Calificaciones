bc0ad26705c0e42df2467e9a6b5660dca1b33a16
Benjamín Guzmán <bg@benjaminguzman.dev>
2020-01-10 06:21:46 +0000

    Added PS "installer" for Windows

---
+++ dist/windows/installer.ps1
@@ -0,0 +1,34 @@
+﻿$CalificacionesPath = "$env:HOMEPATH\Calificaciones"
+$CalificacionesJarPath = "$CalificacionesPath\Calificaciones.jar"
+
+# Copy JAR to $CalificacionesPath
+
+Write-Host "Copying " -NoNewLine; Write-Host "Calificaciones.jar" -NoNewline -f Gray; Write-Host " to " -NoNewline; Write-Host -NoNewline; Write-Host $CalificacionesJarPath -f Gray;
+
+if (!(Test-Path -Path $CalificacionesPath -PathType Container)) {
+    Write-Host "Creating directory " -NoNewline; Write-Host $CalificacionesPath -f Cyan;
+    mkdir $CalificacionesPath
+} else {
+    Write-Host "Directory " -NoNewline; Write-Host $CalificacionesPath -NoNewline -f Cyan; Write-Host " already exists" -f Green;
+}
+
+copy Calificaciones.jar $CalificacionesJarPath
+copy Calificaciones.jpg $CalificacionesPath
+copy Calificaciones.ico $CalificacionesPath
+
+# Create shortcut in desktop to copied JAR
+
+$CalificacionesShortcutPath = "$env:HOMEPATH\Desktop\Calificaciones.lnk"
+
+Write-Host "Creating shortcut in " -NoNewline; Write-Host $CalificacionesShortcutPath -f Yellow;
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+$ShortcutObject = $WScriptShell.CreateShortcut($CalificacionesShortcutPath)
+$ShortcutObject.TargetPath = $CalificacionesJarPath
+$ShortcutObject.Description = "Calificaciones"
+$ShortcutObject.IconLocation = "%SystemRoot%\System32\SHELL32.dll,93"
+$ShortcutObject.Save()
+
+Write-Host "Shortcut created, you can now use the program by clicking " -NoNewLine; Write-Host $CalificacionesShortcutPath -f Yellow
+
+pause
---
+++ dist/windows/uninstaller.ps1
@@ -0,0 +1,26 @@
+﻿$CalificacionesPath = "$env:HOMEPATH\Calificaciones"
+$CalificacionesShortcutPath = "$env:HOMEPATH\Desktop\Calificaciones.lnk"
+
+# Remove JAR from CalificacionesPath
+
+Write-Host "Removing Program directory" -NoNewLine; Write-Host $CalificacionesPath -f Gray;
+
+if (Test-Path -Path $CalificacionesPath -PathType Container) {
+    rm -r $CalificacionesPath
+    Write-Host "Done." -ForegroundColor Green;
+} else {
+    echo "It seems Calificaciones has already been removed"
+}
+
+# Remove shortcut from Desktop
+
+Write-Host "Removing shortcut from Desktop" -NoNewLine; Write-Host $CalificacionesShortcutPath -f Gray;
+
+if (Test-Path -Path $CalificacionesShortcutPath -PathType Leaf) {
+    rm $CalificacionesShortcutPath
+    Write-Host "Done." -ForegroundColor Green;
+} else {
+    echo "It seems desktop shortcut was already removed"
+}
+
+pause
